e464534d00557887cb4a07eb9e936c5282ed0d8c
Alvaro Corral <alvaro.corral@innocv.com>
2020-04-28 09:07:03 +0200

    basic navigation

--- src/server.js
+++ src/server.js
@@ -23,9 +23,7 @@ app.use(express.urlencoded({extended:false}))
 
 //Routes
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(require('./routes/index.routes'))
 
 //Static
 app.use(express.static(path.join(__dirname,'public')) )
