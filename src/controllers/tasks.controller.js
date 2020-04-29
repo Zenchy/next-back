0180e517800e8db121b13a0625dc272f5b145649
Alvaro Corral <alvaro.corral@innocv.com>
2020-04-30 01:43:50 +0200

    form created

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,21 +1,27 @@
 const tasksCtrl = {}
+const Task = require('../models/Task')
 
 tasksCtrl.renderTaskForm = (req,res) => {
   res.render('tasks/new-task')
 }
 
-tasksCtrl.createNewTask = (req, res) => {
-console.log(req.body)
-res.send('ihihihih')
+tasksCtrl.createNewTask = async(req, res) => {
+const { title, description } = req.body
+const newTask = new Task({title, description})
+console.log(newTask)
+await newTask.save()
+res.send('newTask')
 }
 
-tasksCtrl.renderTasks = (req, res) => res.send('All tasks')
+tasksCtrl.renderTasks = async (req, res) => {
+  const tasks = await Task.find()
+  res.render('tasks/all-tasks', {tasks})
+}
 
 tasksCtrl.renderEditTasks = (req, res) => res.send('Edited tasks')
 
 tasksCtrl.renderUpdateTask = (req, res) => res.send('Updated tasks')
 
 tasksCtrl.renderDeleteTask = (req, res) => res.send('Deleted tasks')
-
 
 module.exports = tasksCtrl
